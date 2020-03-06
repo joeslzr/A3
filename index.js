15cf504218e918bec01cf895e9e421cb765214e5
joeslzr <38015581+joeslzr@users.noreply.github.com>
2020-03-06 01:51:12 -0700

    Fixed CSS serving

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var app = require('express')();
+var express = require('express'); //like #include
+var app = express();  // include this no parameter function in express
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+
+
+app.use(express.static(__dirname)); // serve the folder index.js is in
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
