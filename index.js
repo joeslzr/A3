413bea3356069d41675292e29f4e9ff09fadeca0
joeslzr <38015581+joeslzr@users.noreply.github.com>
2020-03-08 03:43:15 -0600

    working chat and user lsit basics

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+/***** SERVER side ******/
+
 var express = require('express'); //like #include
 var app = express();  // include this no parameter function in express
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-
+var userlist = [];
 var userCtr = 0;
 
 //TODO
@@ -21,9 +23,27 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     userCtr += 1;
+
+
     console.log('a user connected');
+
+    socket.emit('userID', userCtr); //send user number/ID to new connected client
+    socket.on('userID', function(username){
+      socket.name = username;
+      userlist.push(socket.name);
+      console.log('users: ' + userlist);
+      io.emit('newUser', userlist); //  send new userlist to all clients
+    });
+    
+    
+    
     socket.on('disconnect', function(){
-      console.log('user disconnected');
+      
+      //todo remove from userlist
+      userlist.splice(userlist.indexOf(socket.name), 1);
+      console.log('user disconnected: ' + socket.name);
+      io.emit('newUser', userlist); //  send new userlist to all clients
+      
     });
   });
 
@@ -31,10 +51,28 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', "User " + userCtr + ": " + msg); // *********
+io.on('connection', function(socket){                           // on "connection" event from client...
+    socket.on('chat message', function(msg, userID){            // on "chat message" event from client...
+        io.emit('chat message', userID + ": " + msg); // we emit the chat message to every client including sender
     });
   });
 
+
+//todo: Event handler for changing username
+// probably a search function to find username in array
+
+//can use this search to delete and rename users
+// function findUser(username){
+//   let found = false;
+//   let i = 0;
+
+//   while(!found || i <= userlist.length){
+//     if(userlist[i] == username){
+//       found = true;
+//     }else{
+//       i++;
+//     }
+//   }
+//   return i;
+// }
 
--- scripts.js
+++ scripts.js
@@ -1,15 +1,21 @@
 
+/********** CLIENT Side ***********/
+
+var socket = io();
+var userID;
+var username;
+
 $(function () {
-  var socket = io();
+  // var socket = io();
   $('form').submit(function(e){
-  e.preventDefault(); // prevents page reloading
-  socket.emit('chat message', $('#m').val());
-  $('#m').val('');
+    e.preventDefault(); // prevents page reloading
+    socket.emit('chat message', $('#m').val(), username);
+    $('#m').val('');
   return false;
   });
   socket.on('chat message', function(msg){
-  $('#messages').append($('<li>').text(msg));
-  autoScroll();
+    $('#messages').append($('<li>').text(msg));
+    autoScroll();
   });
 
 
@@ -18,9 +24,32 @@ $(function () {
 function autoScroll(){
     var element = document.getElementById("chat-window");
     element.scrollTop = element.scrollHeight;
-
-    //element.scrollTo(0,document.querySelector(".chat-window").scrollHeight);
 } 
+
+socket.on('userID', function(userCtr){
+  username = 'user' + userCtr;
+  socket.emit('userID', username);
+
+  console.log('my user ID is: ' + username);
+
+  socket.on('newUser', function(userlist){
+    console.log('userlist: ' + userlist);
+    $('#users-list').empty();
+    userlist.forEach(function(val, i, arr){
+      $('#users-list').append($('<li>').text(val));
+    });
+  });
+});
+
+
+socket.emit('disconnect', username);
+
+
+
+
+
+
+
   
-  
+ //todo: change username 
  //todo: highlighting own messages
